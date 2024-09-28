7d923ea715e5aec2d2da37907fe95c686d716b26
Matthew <git@tekslice.com>
2024-09-27 22:08:25 -0500

    Re-enable curl commands to add zellij and lazygit

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-#curl https://copr.fedorainfracloud.org/coprs/varlad/zellij/repo/fedora-$(rpm -E %fedora)/varlad-zellij-fedora-$(rpm -E %fedora).repo > /etc/yum.repos.d/_copr_varlad-zellij.repo
-#curl https://copr.fedorainfracloud.org/coprs/atim/lazygit/repo/fedora-$(rpm -E %fedora)/atim-lazygit-fedora-$(rpm -E %fedora).repo > /etc/yum.repos.d/_copr_atim-lazygit.repo
+curl https://copr.fedorainfracloud.org/coprs/varlad/zellij/repo/fedora-$(rpm -E %fedora)/varlad-zellij-fedora-$(rpm -E %fedora).repo > /etc/yum.repos.d/_copr_varlad-zellij.repo
+curl https://copr.fedorainfracloud.org/coprs/atim/lazygit/repo/fedora-$(rpm -E %fedora)/atim-lazygit-fedora-$(rpm -E %fedora).repo > /etc/yum.repos.d/_copr_atim-lazygit.repo
 
 # this installs a package from fedora repos
 #rpm-ostree install neovim pass git jq yq bat fzf ripgrep htop eza stow syncthing ansible lazygit zellij
