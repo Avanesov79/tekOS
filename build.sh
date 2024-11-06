12a2bf2b60654d47a355bca2a22005afc0311e4d
Matthew <git@tekslice.com>
2024-11-06 16:40:41 -0600

    Fixing error

--- build.sh
+++ build.sh
@@ -22,6 +22,7 @@ rpm-ostree install --idempotent --allow-inactive pass git jq yq bat fzf ripgrep 
 
 #### Example for enabling a System Unit File
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm --no-start-daemon
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
+    sh -s -- install --determinate --no-confirm --no-start-daemon
 
 systemctl enable podman.socket
