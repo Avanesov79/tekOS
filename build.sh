3cbdc94297985b699c5a317481a3a387885af00d
Matthew <git@tekslice.com>
2024-11-06 15:34:14 -0600

    Disabled starting the nix daemon

--- build.sh
+++ build.sh
@@ -22,6 +22,6 @@ rpm-ostree install --idempotent --allow-inactive pass git jq yq bat fzf ripgrep 
 
 #### Example for enabling a System Unit File
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate --no-confirm --no-start-daemon
 
 systemctl enable podman.socket
