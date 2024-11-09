49065bdb84f69ff597e75e7a37163713a99e22bd
Matthew <git@tekslice.com>
2024-11-09 13:47:24 -0600

    disabled all package installation in favor of nix

--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-curl https://copr.fedorainfracloud.org/coprs/varlad/zellij/repo/fedora-$(rpm -E %fedora)/varlad-zellij-fedora-$(rpm -E %fedora).repo > /etc/yum.repos.d/_copr_varlad-zellij.repo
-curl https://copr.fedorainfracloud.org/coprs/atim/lazygit/repo/fedora-$(rpm -E %fedora)/atim-lazygit-fedora-$(rpm -E %fedora).repo > /etc/yum.repos.d/_copr_atim-lazygit.repo
+#curl https://copr.fedorainfracloud.org/coprs/varlad/zellij/repo/fedora-$(rpm -E %fedora)/varlad-zellij-fedora-$(rpm -E %fedora).repo > /etc/yum.repos.d/_copr_varlad-zellij.repo
+#curl https://copr.fedorainfracloud.org/coprs/atim/lazygit/repo/fedora-$(rpm -E %fedora)/atim-lazygit-fedora-$(rpm -E %fedora).repo > /etc/yum.repos.d/_copr_atim-lazygit.repo
 
 # this installs a package from fedora repos
-rpm-ostree install --idempotent --allow-inactive pass git jq yq bat fzf ripgrep htop eza stow syncthing ansible lazygit zellij neovim emacs
+#rpm-ostree install --idempotent --allow-inactive pass git jq yq bat fzf ripgrep htop eza stow syncthing ansible lazygit zellij neovim emacs
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
